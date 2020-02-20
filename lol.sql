5ddb674b6fa9c37ce560912aef1acec6b8c45b17
FearBlackScarab <fearblackscarab@gmail.com>
2020-02-19 20:17:04 -0600

    removed id for all tables

--- lol.sql
+++ lol.sql
@@ -2,12 +2,11 @@ drop database lol;
 create database lol;
 
 use lol;
-create table meme (id int, name varchar(20), creator varchar(20));
-create table gif (id int, name varchar(20), creator varchar(20));
-create table hashtag (id int, name varchar(26));
+create table meme (name varchar(20), creator varchar(20));
+create table gif (name varchar(20), creator varchar(20));
+create table hashtag (name varchar(26));
 
 insert into meme(name, creator) values ('minion', 'bob');
 insert into gif(name, creator) values ('minion', 'bob');
 insert into hashtag(name) values ('bob');
 insert into hashtag(name) values ('minion');
-
